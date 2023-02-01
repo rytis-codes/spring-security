c00ba8104f12857dc6c84a26018a84be58d893d9
Rytis Tekorius <rytis@tekorius.com>
2023-02-02 00:54:05 +0200

    Initial commit

---
+++ src/main/java/com/example/logindemo/LoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.logindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logindemo/LoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
