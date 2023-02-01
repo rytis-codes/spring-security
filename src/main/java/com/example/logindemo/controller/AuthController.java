39496de66b7104e375a9640c3a6c63c4d70506af
Rytis Tekorius <rytis@tekorius.com>
2023-02-02 01:42:17 +0200

    Implement JWT issuer

--- src/main/java/com/example/logindemo/controller/AuthController.java
+++ src/main/java/com/example/logindemo/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.logindemo.controller;
 
 import com.example.logindemo.model.LoginRequest;
 import com.example.logindemo.model.LoginResponse;
+import com.example.logindemo.security.JwtIssuer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,14 +10,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final JwtIssuer jwtIssuer;
+
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) {
+        var token = jwtIssuer.issue(JwtIssuer.Request.builder()
+                .userId(1L)
+                .email("test@test.com")
+                .roles(List.of("ROLE_ADMIN"))
+                .build());
+
         return LoginResponse.builder()
-                .token("Bla bla bla")
+                .token(token)
                 .build();
     }
 }
