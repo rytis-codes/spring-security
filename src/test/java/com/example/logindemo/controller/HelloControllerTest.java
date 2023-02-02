7f454f0948e48a9d9d8656b47a21205225e3a9e2
Rytis Tekorius <rytis@tekorius.com>
2023-02-02 20:40:03 +0200

    Add basic HelloController test

---
+++ src/test/java/com/example/logindemo/controller/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.logindemo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    @Autowired
+    private MockMvc api;
+
+    @Test
+    void anyoneCanViewPublicEndpoint() throws Exception {
+        api.perform(get("/public"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Everyone can see this"));
+    }
+
+    @Test
+    void notLoggedIn_shouldNotSeeSecuredEndpoint() throws Exception {
+        api.perform(get("/securued"))
+                .andExpect(status().isUnauthorized());
+    }
+}
