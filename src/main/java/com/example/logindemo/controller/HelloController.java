933f29b9cab166542a78c4643fd446714efb3ac9
Rytis Tekorius <rytis@tekorius.com>
2023-02-02 02:00:45 +0200

    Make secure endpoint return user email and id

--- src/main/java/com/example/logindemo/controller/HelloController.java
+++ src/main/java/com/example/logindemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.logindemo.controller;
 
+import com.example.logindemo.security.UserPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,8 @@ public class HelloController {
     }
 
     @GetMapping("/secured")
-    public String secured() {
-        return "This can only be seen by a logged in user";
+    public String secured(@AuthenticationPrincipal UserPrincipal principal) {
+        return "This can only be seen by a logged in user. Your Email is: "
+                + principal.getEmail() + " your ID: " + principal.getUserId();
     }
 }
