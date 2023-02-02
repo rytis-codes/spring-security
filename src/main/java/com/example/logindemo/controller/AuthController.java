be9fc5e7a82c5a38eed068c32cd960e958b4cca0
Rytis Tekorius <rytis@tekorius.com>
2023-02-02 19:53:53 +0200

    Refactor login from controller to its own AuthService

--- src/main/java/com/example/logindemo/controller/AuthController.java
+++ src/main/java/com/example/logindemo/controller/AuthController.java
@@ -2,13 +2,8 @@ package com.example.logindemo.controller;
 
 import com.example.logindemo.model.LoginRequest;
 import com.example.logindemo.model.LoginResponse;
-import com.example.logindemo.security.JwtIssuer;
-import com.example.logindemo.security.UserPrincipal;
+import com.example.logindemo.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,25 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final JwtIssuer jwtIssuer;
-    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) {
-        var authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        var principal = (UserPrincipal) authentication.getPrincipal();
-
-        var token = jwtIssuer.issue(JwtIssuer.Request.builder()
-                .userId(principal.getUserId())
-                .email(principal.getEmail())
-                .roles(principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
-                .build());
-
-        return LoginResponse.builder()
-                .token(token)
-                .build();
+        return authService.attemptLogin(request.getEmail(), request.getPassword());
     }
 }
